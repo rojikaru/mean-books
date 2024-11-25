12cc2bdadbd18d7f6fcc1fdd16e00ead7276076d
Alex Ivanov <oleksii120@gmail.com>
2024-11-25 20:48:52 +0200

    Register routes at frontend

--- books-client/src/app/app.routes.ts
+++ books-client/src/app/app.routes.ts
@@ -1,3 +1,34 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {BooksComponent} from './books/books.component';
+import {BookComponent} from './book/book.component';
+import {CreateBookComponent} from './create-book/create-book.component';
+import {EditBookComponent} from './edit-book/edit-book.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    "path": "",
+    "title": "Home",
+    "component": HomeComponent,
+  },
+  {
+    "path": "books",
+    "title": "Books",
+    "component": BooksComponent,
+  },
+  {
+    "path": "books/:id",
+    "title": "Book Details",
+    "component": BookComponent,
+  },
+  {
+    "path": "books/new",
+    "title": "Add Book",
+    "component": CreateBookComponent,
+  },
+  {
+    "path": "books/:id/edit",
+    "title": "Edit Book",
+    "component": EditBookComponent,
+  },
+];
