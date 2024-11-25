32a276e6504985fe46cf2c79e49bc0eef474fc11
Alex Ivanov <oleksii120@gmail.com>
2024-11-25 21:12:14 +0200

    Add books component

---
+++ books-client/src/app/components/books/books.component.ts
@@ -0,0 +1,40 @@
+import {Component, OnInit} from '@angular/core';
+import {BookService} from '@/services/book.service';
+import Book from '@/types/book.interface';
+import {RouterLink} from '@angular/router';
+import {NgForOf} from '@angular/common';
+
+@Component({
+  selector: 'app-books',
+  templateUrl: './books.component.html',
+  styleUrls: ['./books.component.css'],
+  imports: [
+    RouterLink,
+    NgForOf,
+  ],
+})
+export class BooksComponent implements OnInit {
+  books: Book[] = [];
+
+  constructor(private bookService: BookService) { }
+
+  ngOnInit() {
+    this.loadBooks();
+  }
+
+  loadBooks() {
+    this.bookService.getBooks()
+      .subscribe({
+        next: (books) => this.books = books,
+        error: (error) => console.error('Error loading books:', error),
+      })
+  }
+
+  deleteBook(id: string) {
+    this.bookService.deleteBook(id)
+      .subscribe({
+        next: () => this.loadBooks(),
+        error: (error) => console.error('Error deleting book:', error),
+      });
+  }
+}
