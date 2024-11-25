28c56bdd8d5f39d924da251f4bc68aa39d8b878f
Alex Ivanov <oleksii120@gmail.com>
2024-11-25 23:26:17 +0200

    Add cors configuration

--- books-server/main.js
+++ books-server/main.js
@@ -1,19 +1,27 @@
 import express from 'express';
+import cors from 'cors';
 import bookRouter from "./routers/book.js";
 import catchAllRouter from "./routers/catch-all.js";
 import handleError from "./middleware/error.js";
 
 async function main() {
     const app = express();
-    const port = +process.env.PORT ?? 3000;
+    const port = +process.env.PORT ?? 80;
 
-    // Register routes
+    // Middleware
+    app.use(cors({
+        origin: '*',
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'HEAD', 'OPTIONS', 'PATCH'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+    }));
+    app.use(express.json());
+    app.use(express.urlencoded({extended: true}));
+
+    // Routes
     app.use('/api/book', bookRouter);
     app.use(catchAllRouter);
 
-    // Register middleware
-    app.use(express.json());
-    app.use(express.urlencoded({extended: true}));
+    // Error handling middleware
     app.use(handleError);
 
     app.listen(port, 'localhost', () => {
